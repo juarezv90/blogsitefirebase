d5b1c1518d701e923135390650f3c9501064d4e9
Victor <juarezv1990@gmail.com>
2023-04-22 19:39:49 -0400

    some updates include test data

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import {FaGithubSquare, FaInstagramSquare, FaLinkedin} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div className='bg-black text-white mt-auto'>Footer</div>
+    <div className='bg-black text-white mt-auto flex justify-center items-center py-3 gap-2 md:gap-4'>
+      <FaLinkedin className='w-[3rem] h-[3rem] md:w-[4rem] md:h-[4rem]'/>
+      <FaGithubSquare className='w-[3rem] h-[3rem] md:w-[4rem] md:h-[4rem]'/>
+      <FaInstagramSquare className='w-[3rem] h-[3rem] md:w-[4rem] md:h-[4rem]'/>
+    </div>
   )
 }
 
